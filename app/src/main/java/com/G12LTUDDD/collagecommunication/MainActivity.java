1f25d286a8a7c588f2e7f6d3b0bc721275cbedb1
Shinchoku <tienmp3@gmail.com>
2022-06-21 13:43:45 +0700

    update fb login

--- app/src/main/java/com/G12LTUDDD/collagecommunication/MainActivity.java
+++ app/src/main/java/com/G12LTUDDD/collagecommunication/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 public class MainActivity extends AppCompatActivity {
     EditText txtEmail, txtPassword;
     ProgressBar progressBar;
